6a761ae79bc7fa58c123e44545446530bdad817d
dstrauss <fmdenver@gmail.com>
2019-07-27 00:15:39 -0400

    ranking Manager page with advanced ranking logic

--- src/fantasy-football-react-app/src/containers/RankingManager/RankingManager.js
+++ src/fantasy-football-react-app/src/containers/RankingManager/RankingManager.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Players from '../../component/Players/Players';
 import axios from 'axios';
+import { debuggerStatement } from '@babel/types';
 
 
 const scoringTypes = ["standard", "ppr", "dynasty"];
@@ -10,12 +11,14 @@ const positionFilters = ["ALL", "QB", "RB", "WR", "TE", "DST", "K"]
 
 class RankingManager extends Component {
 
-
-    state = {
-        scoringType: scoringTypes[0],
-        positionFilter: positionFilters[0],
-        playerRankings: null,
-        isLoading: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            scoringType: scoringTypes[0],
+            positionFilter: positionFilters[0],
+            playerRankings: null,
+            isLoading: true
+        };
     }
 
     componentDidMount() {
@@ -30,21 +33,117 @@ class RankingManager extends Component {
             })
     }
 
-    scoringChangeHandler(scoretype){
-       this.setState({ scoringType: scoringTypes[scoretype] })
+    scoringChangeHandler = (scoretype) => {
+        this.setState({ scoringType: scoringTypes[scoretype] })
     }
-    posChangeHandler(posfiltertype){
+    posChangeHandler = (posfiltertype) => {
         this.setState({ positionFilter: positionFilters[posfiltertype] })
-     }
+    }
 
-     moveUpClickedHandler()
-     {
-         alert("Up!")
-     }
-     moveDownClickedHandler()
-     {
-         alert("Down!")
-     }
+
+    movePlayerClickedHandler = (playerId, dir) => {
+        let playerToChange = this.state.playerRankings.filter(player => player.playerToRank.playerId === playerId);
+        let initialRanks;
+        let allPlayersInPos;
+        let otherPlayerToChange;
+        let otherPlayersRanks;
+        console.log(playerToChange);
+        switch (this.state.scoringType) {
+            case "standard":
+                initialRanks = [playerToChange[0].playerRanking.playerRank, playerToChange[0].playerRanking.posRank];
+                break;
+            case "ppr":
+                initialRanks = [playerToChange[0].playerRanking.pprRank, playerToChange[0].playerRanking.pprPosRank];
+                break;
+            case "dynasty":
+                initialRanks = [playerToChange[0].playerRanking.dynastyRank, playerToChange[0].playerRanking.dynastyPosRank];
+                break;
+            default:
+                initialRanks = [playerToChange[0].playerRanking.playerRank, playerToChange[0].playerRanking.posRank];
+                break;
+        }
+        console.log(initialRanks);
+
+        if (this.state.positionFilter === "ALL") {
+            switch (this.state.scoringType) {
+                case "standard":
+                    otherPlayerToChange = this.state.playerRankings.filter(player => player.playerRanking.playerRank === (initialRanks[0] + dir));
+                    otherPlayersRanks = [otherPlayerToChange[0].playerRanking.playerRank, otherPlayerToChange[0].playerRanking.posRank];
+                    otherPlayerToChange[0].playerRanking.playerRank = initialRanks[0];
+                    playerToChange[0].playerRanking.playerRank = otherPlayersRanks[0];
+                    if (otherPlayerToChange[0].playerToRank.playerPos === playerToChange[0].playerToRank.playerPos) {
+                        otherPlayerToChange[0].playerRanking.posRank = initialRanks[1];
+                        playerToChange[0].playerRanking.posRank = otherPlayersRanks[1];
+                    }
+                    break;
+                case "ppr":
+                    otherPlayerToChange = this.state.playerRankings.filter(player => player.playerRanking.pprRank === (initialRanks[0] + dir));
+                    otherPlayersRanks = [otherPlayerToChange[0].playerRanking.pprRank, otherPlayerToChange[0].playerRanking.pprPosRank];
+                    otherPlayerToChange[0].playerRanking.pprRank = initialRanks[0];
+                    playerToChange[0].playerRanking.pprRank = otherPlayersRanks[0];
+                    if (otherPlayerToChange[0].playerToRank.playerPos === playerToChange[0].playerToRank.playerPos) {
+                        otherPlayerToChange[0].playerRanking.pprPosRank = initialRanks[1];
+                        playerToChange[0].playerRanking.pprPosRank = otherPlayersRanks[1];
+                    }
+                    break;
+                case "dynasty":
+                    otherPlayerToChange = this.state.playerRankings.filter(player => player.playerRanking.dynastyRank === (initialRanks[0] + dir));
+                    otherPlayersRanks = [otherPlayerToChange[0].playerRanking.dynastyRank, otherPlayerToChange[0].playerRanking.dynastyPosRank];
+                    otherPlayerToChange[0].playerRanking.dynastyRank = initialRanks[0];
+                    playerToChange[0].playerRanking.dynastyRank = otherPlayersRanks[0];
+                    if (otherPlayerToChange[0].playerToRank.playerPos === playerToChange[0].playerToRank.playerPos) {
+                        otherPlayerToChange[0].playerRanking.dynastyPosRank = initialRanks[1];
+                        playerToChange[0].playerRanking.dynastyPosRank = otherPlayersRanks[1];
+                    }
+                    break;
+                default:
+                    console.log("something went wrong!");
+            }
+        }
+        if (this.state.positionFilter !== "ALL") {
+            allPlayersInPos = this.state.playerRankings.filter(player => player.playerToRank.playerPos === this.state.positionFilter);
+            switch (this.state.scoringType) {
+                case "standard":
+                    otherPlayerToChange = allPlayersInPos.filter(player => player.playerRanking.posRank === (initialRanks[1] + dir));
+                    otherPlayersRanks = [otherPlayerToChange[0].playerRanking.playerRank, otherPlayerToChange[0].playerRanking.posRank];
+                    otherPlayerToChange[0].playerRanking.playerRank = initialRanks[0];
+                    playerToChange[0].playerRanking.playerRank = otherPlayersRanks[0];
+                    otherPlayerToChange[0].playerRanking.posRank = initialRanks[1];
+                    playerToChange[0].playerRanking.posRank = otherPlayersRanks[1];
+                    break;
+                case "ppr":
+                    otherPlayerToChange = allPlayersInPos.filter(player => player.playerRanking.pprPosRank === (initialRanks[1] + dir));
+                    otherPlayersRanks = [otherPlayerToChange[0].playerRanking.pprRank, otherPlayerToChange[0].playerRanking.pprPosRank];
+                    otherPlayerToChange[0].playerRanking.pprRank = initialRanks[0];
+                    playerToChange[0].playerRanking.pprRank = otherPlayersRanks[0];
+                    otherPlayerToChange[0].playerRanking.pprPosRank = initialRanks[1];
+                    playerToChange[0].playerRanking.pprPosRank = otherPlayersRanks[1];
+                    break;
+                case "dynasty":
+                    otherPlayerToChange = allPlayersInPos.filter(player => player.playerRanking.dynastyPosRank === (initialRanks[1] + dir));
+                    otherPlayersRanks = [otherPlayerToChange[0].playerRanking.dynastyRank, otherPlayerToChange[0].playerRanking.dynastyPosRank];
+                    otherPlayerToChange[0].playerRanking.dynastyRank = initialRanks[0];
+                    playerToChange[0].playerRanking.dynastyRank = otherPlayersRanks[0];
+                    otherPlayerToChange[0].playerRanking.dynastyPosRank = initialRanks[1];
+                    playerToChange[0].playerRanking.dynastyPosRank = otherPlayersRanks[1];
+                    break;
+                default:
+                    console.log("something went wrong!");
+            }
+        }
+        console.log(playerToChange);
+        console.log(otherPlayerToChange);
+        this.setState({playerToChange,otherPlayerToChange})
+
+    }
+
+
+
+
+    moveDownClickedHandler = (playerId) => {
+        const playerToChange = this.state.playerRankings.filter(player => player.playerToRank.playerId === playerId);
+        console.log(playerToChange);
+    }
 
     render() {
 
@@ -72,12 +171,11 @@ class RankingManager extends Component {
                     <button onClick={() => this.posChangeHandler(5)}>DST</button>
                     <button onClick={() => this.posChangeHandler(6)}>K</button>
                     <main>
-                    <Players
-                        playersToRank={this.state.playerRankings}
-                        playerScoringType={this.state.scoringType}
-                        playerPositionFilter={this.state.positionFilter}
-                        moveUpClicked={this.moveUpClickedHandler} 
-                        moveDownClicked = {this.moveDownClickedHandler}/>
+                        <Players
+                            playersToRank={this.state.playerRankings}
+                            playerScoringType={this.state.scoringType}
+                            playerPositionFilter={this.state.positionFilter}
+                            movePlayerClicked={this.movePlayerClickedHandler} />
                     </main>
                 </Aux>
             )

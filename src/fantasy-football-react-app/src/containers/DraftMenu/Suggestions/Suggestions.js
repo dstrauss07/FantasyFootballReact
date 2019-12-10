b64921ea663355a2dd46fd2038daf1c0a38fd2c7
dstrauss <fmdenver@gmail.com>
2019-12-09 21:08:35 -0500

    fixed the suggestions

---
+++ src/fantasy-football-react-app/src/containers/DraftMenu/Suggestions/Suggestions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Aux from '../../../hoc/ReactAux';
+import Classes from './Suggestions.module.css';
+
+const Suggestions  = () =>
+{
+
+  return (
+    <Aux>
+
+      <h4>Suggestions</h4>
+
+
+    </Aux>
+)
+
+  }
+  
+export default Suggestions;
+
+  
